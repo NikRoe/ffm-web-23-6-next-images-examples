e7d071389541fe40addb77a3c49667dfaf4fc092
Niklas Röttger <niklas.roettger@outlook.de>
2024-01-19 10:33:35 +0100

    add next images examples

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tolkiengateway.net',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
